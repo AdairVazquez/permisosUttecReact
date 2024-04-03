7e374f36e7bcf2d8e76a7608c325f2632aa39b74
Alan Adair Vazquez Cruz <adairvazquez@MacBook-Pro-de-Alan.local>
2024-04-03 01:20:20 -0600

    actualizado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Login from './components/Login';
-import Permisos from './components/Permisos';
-import Menu from './components/Menu';
 import Home from './components/Home';
 import Principal from './components/Principal';
 import NuevoPermiso from './components/NuevoPermiso';
-import React, { Children, Component, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { UseSelector, useSelector } from 'react-redux';
-import logo from './logo.svg';
-import { BrowserRouter, Route, Routes, Redirect, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Pdf from './components/Pdf';
 
 function App() {
